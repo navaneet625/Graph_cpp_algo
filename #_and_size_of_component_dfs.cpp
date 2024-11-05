05fd3435f14906b45d1af47fa5bf688a9a63a0ed
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-05 16:50:08 +0530

    Update #_and_size_of_component_dfs.cpp

--- #_and_size_of_component_dfs.cpp
+++ #_and_size_of_component_dfs.cpp
@@ -1,39 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<int>adj[], vector<int>&vis, int u, int &size)
-{
-	vis[u] = true;
+void dfs(unordered_map<int,vector<int>>&adj,unordered_set<int>&vis, int u, int &size){
+	// vis[u] = true;
+	vis.insert(u);
 	// cout << u << " ";
 	size++;
-	for (int v : adj[u])
-	{
-		if (!vis[v])
+	for (int v : adj[u]){
+		if (vis.find(v)==vis.end())
 			dfs(adj, vis, v, size);
 	}
 }
-int main()
-{
+int main(){
 	int n, m;
 	cin >> n >> m;
-	vector<int> adj[n + 1];
-	for (int i = 0; i < m; i++)
-	{
+	// vector<int> adj[n + 1];
+	unordered_map<int , vector<int>> adj;
+	for (int i = 0; i < m; i++){
 		int a, b;
 		cin >> a >> b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);//if graph is undirected
 	}
-	vector<int> vis(n + 1, false);
+	// vector<int> vis(n + 1, false);
+	unordered_set<int>vis
 	int count = 0;
 	vector<int> S;
-	for (int i = 0; i < n; i++)
-	{
-		if (vis[i] == false)
-		{
+	for (auto x:adj){
+		if (vis.find(x.first) == false){
 			count++;
 			int size = 0;
-			dfs(adj, vis, i, size);
+			dfs(adj, vis, x.first, size);
 			S.push_back(size);
 		}
 	}
