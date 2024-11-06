97ca99f1088a050d19260ae8171523a1d7d8fc95
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-06 17:21:30 +0530

    Update detect_cycle_DG_bfs.cpp

--- detect_cycle_DG_bfs.cpp
+++ detect_cycle_DG_bfs.cpp
@@ -1,49 +1,62 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
 using namespace std;
 
+int main() {
+    int n, e; // number of nodes and edges
+    cin >> n >> e;
+    
+    unordered_map<int, vector<int>> adj;
+    int a, b;
+    
+    for (int i = 0; i < e; i++) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+    }
 
-int main()
-{
-	int n, m;
-	cin >> n >> m;
+    vector<int> inDegree(n, 0);
+    
+    // Calculate in-degrees of all nodes
+    for (auto x : adj) {
+        for (auto y : x.second) {
+            inDegree[y]++;
+        }
+    }
 
-	vector<vector<int>>adj(n);
+    // Initialize queue with all nodes having in-degree 0
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-	}
+    int count = 0; // Count of nodes processed in topological sort order
+    
+    // Perform topological sort
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        
+        // Process the node
+        count++;
+        
+        for (auto x : adj[node]) {
+            inDegree[x]--;
+            if (inDegree[x] == 0) {
+                q.push(x);
+            }
+        }
+    }
 
-	vector<int>inDegree(n, 0);
-	for (int i = 0; i < n; i++)
-	{
-		for (auto x : adj[i])
-			inDegree[x]++;
-	}
+    // If count is not equal to n, there is a cycle
+    if (count != n) {
+        cout << "Yes\n";  // Cycle detected
+    } else {
+        cout << "No\n";   // No cycle
+    }
 
-	queue<int> q;
-	for (int i = 0; i < n; i++)
-	{
-		if (inDegree[i] == 0)
-			q.push(i);
-	}
-
-	int count = 0;
-	while (!q.empty())
-	{
-		int node = q.front();
-		q.pop();
-		// cout<<node<<"<<";
-		for (auto x : adj[node])
-		{
-			inDegree[x]--;
-			if (inDegree[x] == 0)
-				q.push(x);
-		}
-		count++;
-	}
-	cout << count << endl;
-	(count != n) ? cout << "Yes \n" : cout << "No \n";
+    return 0;
 }
