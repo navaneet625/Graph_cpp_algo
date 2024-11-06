4c6fd804fd96ef3c7cf46882d6dd195ff57efd22
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-06 16:17:13 +0530

    Update detect_cycle_UG_dfs.cpp

--- detect_cycle_UG_dfs.cpp
+++ detect_cycle_UG_dfs.cpp
@@ -1,42 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool dfs(vector<vector<int>> & adj, vector<bool> &vis, int node, int parent)
-{
-	vis[node] = true;
-	for (auto x : adj[node])
-	{
-		if (!vis[x] && dfs(adj, vis, x, node))
-			return true;
-		else if (x != parent)
+bool dfs(unordered_map<int,vector<int>> & adj, vector<bool> &vis, int node, int parent){
+	// vis[node] = true;
+	vis.insert(node);
+	for (auto x : adj[node]){
+		if(x==parent) continue;
+		if(vis.find(x)!=vis.end()) return true;
+		if (dfs(adj, vis, x, node))
 			return true;
 	}
 	return false;
 }
 
-int main()
-{
-	int n, m;
+int main(){
+	int n, m,a,b;
 	cin >> n >> m;
 
-	vector<vector<int>> adj(n);
+	// vector<vector<int>> adj(n);
+	unordered_map<int,vector<int>>adj
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
+	for (int i = 0; i < m; i++){
 		cin >> a >> b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
-	vector<bool> vis(n, false);
+	// vector<bool> vis(n, false);
+	unordered_set<int>vis;
 
-	int src;
-	cin >> src;
 	bool ans = false;
-	for (int i = 0; i < n; i++)
-	{
-		if (!vis[i] && dfs(adj, vis, src, -1))
-		{
+	for (auto x:adj){
+		if (vis.find(x.first)==vis.end() && dfs(adj, vis, x.first, -1)){
 			ans = true;
 			break
 		}
