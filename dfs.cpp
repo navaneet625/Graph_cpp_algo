bc4e8052a337b014bd488eed8520966d1da2e21d
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-06 14:41:11 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,28 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-void dfs(vector<int>adj[], vector<int>&vis, int u)
-{
-	vis[u] = true;
+void dfs(unordered_map<int,vector<int>>&adj, unordered_set<int>&vis, int u){
+	// vis[u] = true;
+	vis.insert(u);
 	cout << u << " ";
-	for (int v : adj[u])
-	{
-		if (!vis[v])
+	for (int v : adj[u]){
+		if (vis.find(v)==vis.end())
 			dfs(adj, vis, v);
 	}
 }
-int main()
-{
+int main(){
 	int n, m;
 	cin >> n >> m;
-	vector<int> adj[n + 1];
-	for (int i = 0; i < m; i++)
-	{
+	// vector<int> adj[n + 1];
+	unordered_map<int,vector<int>>adj;
+	for (int i = 0; i < m; i++){
 		int a, b;
 		cin >> a >> b;
 		adj[a].push_back(b);
-		//adj[b].push_back(a);//if graph is undirected
+		adj[b].push_back(a);//if graph is undirected
 	}
-	vector<int> vis(n + 1, false);
+	// vector<int> vis(n + 1, false);
+	unordered_set<int> vis
 	dfs(adj, vis, 1);
 	return 0;
 }
