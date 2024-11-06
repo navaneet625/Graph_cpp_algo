5d0a9d42f86f1290e1ab84c0919edbc851d2fe45
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-06 14:45:49 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,34 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
 
 	int n_nodes, n_edges;
 	cin >> n_nodes >> n_edges;
-	vector<vector<int>> adj(n_nodes + 1);
-	for (int i = 0; i < n_edges; i++)
-	{
-		int src, dst;
-		cin >> src >> dst;
-		adj[src].push_back(dst);
-		//adj[b].push_back(a)//if graph is undirected
+	// vector<vector<int>> adj(n_nodes + 1);
+	unordered_map<int,vector<int>> adj;
+	for (int i = 0; i < n_edges; i++){
+		int a,b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a)//if graph is undirected
 	}
 	queue<int> Q;
-	vector<bool> visited(n_nodes + 1, false);
-	int source = 1;
-	visited[source] = true;
-
+	// vector<bool> visited(n_nodes + 1, false);
+	unordered_set<int>visited;
+	int source;
+	cin>>source; // it should be the name of existing node 
+	visited.insert(source);
 	Q.push(source);
-	while (!Q.empty())
-	{
+	while (!Q.empty()){
 		int node = Q.front();
 		Q.pop();
 		cout << node << " ";
-		for (int neighbour : adj[node])
-		{
-			if (visited[neighbour])
+		for (int neighbour : adj[node]){
+			if (visited.find(neighbour)==visisted.end())
 				continue;
-			visited[neighbour] = true;
+			// visited[neighbour] = true/
+			visited.insert(neighbour);
 			Q.push(neighbour);
 		}
 	}
