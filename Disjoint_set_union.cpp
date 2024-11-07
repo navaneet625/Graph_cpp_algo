9b7a09f177b7dbf2ef18179c7a130cb5f918d0cd
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-07 13:10:56 +0530

    Update Disjoint_set_union.cpp

--- Disjoint_set_union.cpp
+++ Disjoint_set_union.cpp
@@ -1,72 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int findSet(int a, vector<int>&parent)
-{
-	if (parent[a] == -1)
-		return a;
-	return parent[a] = findSet(parent[a], parent);
+int findSet(int a, vector<int>& parent) {
+    if (parent[a] == -1)
+        return a;
+    return parent[a] = findSet(parent[a], parent);  // Path compression
 }
 
-void union_set(int x, int y, vector<int>&parent, vector<int>&rank)
-{
-	int s1 = findSet(x, parent);
-	int s2 = findSet(y, parent);
-
-	if (s1 != s2)
-	{
-		if (rank[s1] < rank[s2])
-		{
-			parent[s1] = s2;
-			rank[s2] += rank[s1];
-		}
-		else
-		{
-			parent[s2] = s1;
-			rank[s1] += rank[s2];
-		}
-	}
-
+void union_set(int s1, int s2, vector<int>& parent, vector<int>& rank) {
+    if (rank[s1] < rank[s2]) {
+        parent[s1] = s2;
+        rank[s2] += rank[s1];
+    } else {
+        parent[s2] = s1;
+        rank[s1] += rank[s2];
+    }
 }
 
-bool contain_cycle(vector<pair<int, int>>&adj, int n)
-{
-	vector<int>parent(n, -1), rank(n, 1);
+bool contain_cycle(vector<pair<int, int>>& adj, int n) {
+    vector<int> parent(n, -1), rank(n, 1);
 
-	for (auto x : adj)
-	{
-		int a = x.first;
-		int b = x.second;
-		int s1 = findSet(a, parent);
-		int s2 = findSet(b, parent);
+    for (auto& edge : adj) {
+        int a = edge.first;
+        int b = edge.second;
+        int s1 = findSet(a, parent);
+        int s2 = findSet(b, parent);
 
-		if (s1 != s2)
-			union_set(s1, s2, parent, rank);
-		else
-			return true;
-
-	}
-	parent.clear();
-	return false;
+        if (s1 == s2)
+            return true;  // Cycle detected
+        else
+            union_set(s1, s2, parent, rank);
+    }
+    return false;
 }
-int main()
-{
-	int n, m;
-	cin >> n >> m;
-	vector<pair<int, int>>adj;
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		adj.push_back(make_pair(a, b));
-	}
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> adj;
 
-	bool ans = contain_cycle(adj, n);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj.push_back({a, b});
+    }
 
-	if (ans)
-		cout << "cyclic graph" << endl;
-	else
-		cout << "Acyclic graph" << endl;
+    bool ans = contain_cycle(adj, n);
 
+    if (ans)
+        cout << "Cyclic graph" << endl;
+    else
+        cout << "Acyclic graph" << endl;
+
+    return 0;
 }
