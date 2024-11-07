c9b5d1ea5e43ad34414f6b33d2b8ce95e158dd3d
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-07 10:19:07 +0530

    Update dijkstra_alg_Adjlist.cpp

--- dijkstra_alg_Adjlist.cpp
+++ dijkstra_alg_Adjlist.cpp
@@ -1,47 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int INF = 10000000;// use any number or INT_MAX
 
-#define pii pair<int,int>
-int main()
-{
-	int n, m;
-	cin >> n >> m;
+int main(){
 
-	vector<vector<pii>> adj(n);
+	int n,m,a,b,wt;
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b, wt;
-		cin >> a >> b >> wt;
-		adj[a].push_back({b, wt});
+	cin>>n>>m;
+
+	unordered_map<int ,vector<pair<int,int>>> adj;
+
+	for(int i=0;i<m;i++){
+		cin>>a>>b>>wt;
+		adj[a].push_back({b,wt});
+		// adj[b].push_back({a,wt});  for undirected graph
 	}
-	vector<int> distance(n, INT_MAX);
-	distance[0] = 0;
 
-	set<pii> s;
+	vector<int>dist(n,INF);
+	vector<int>parent(n,-1);
 
-	s.insert({0, 0});//dest,node
+	set<pair<int,int>>s;
+	int src;
+	cin>>src;
 
-	while (!s.empty())
-	{	auto node  = *s.begin();
-		int v = node.second;
-		int wt = node.first;
+	dist[src]=0;
+
+	s.insert({0,src});
+
+	while(!s.empty()){
+		int v = s.begin()->second;
 		s.erase(s.begin());
-		for (auto x : adj[v])
-		{
-			if (wt + x.second < distance[x.first])
-			{
-				auto itr = s.find({distance[x.first], x.first});
-				if (itr != s.end())
-					s.erase(itr);
-				distance[x.first] = wt + x.second;
-				s.insert({distance[x.first], x.first});
-			}
 
+		for(auto x:adj[v]){
+			int u = x.first;
+			int weight = x.second;
+
+			if(dist[v]+weight<dist[u]){
+
+				if(s.find({dist[u],u})!=s.end())
+					s.erase({dist[u],u});
+				dist[u]=dist[v]+weight;
+				parent[u]=v;
+				s.insert({dist[u],u});
+			}
 		}
 	}
-	int i = 0;
-	for (auto x : distance)
-		cout << "vertex(0)-->" << i++ << " " << x << endl;
-	return 0;
+
+	for (int i = 0; i < n; i++) {
+        cout << "Distance from " << src << " to " << i << " is " << dist[i] << endl;
+    }
+    return 0;
 }
