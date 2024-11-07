8f6e595b413cf03aa785f00bf38c3c65bb67884e
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-07 14:35:53 +0530

    Update kruskal_mst.cpp

--- kruskal_mst.cpp
+++ kruskal_mst.cpp
@@ -1,73 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int find(int i, vector<int>&parent)
-{
-	if (parent[i] == -1)
-		return i;
-	return parent[i] = find(parent[i], parent);
+int find_set(int a, vector<int>& parent) {
+    if (parent[a] == -1)
+        return a;
+    return parent[a] = find_set(parent[a], parent);  // Path compression
 }
 
-void Union(int x, int y, vector<int>&parent, vector<int>&rank)
-{
-	int s1 = find(x, parent);
-	int s2 = find(y, parent);
-
-	if (s1 != s2)
-	{
-		if (rank[s1] < rank[s2])
-		{
-			rank[s2] += rank[s1];
-			parent[s1] = s2;
-		}
-		else
-		{
-			rank[s1] += rank[s2];
-			parent[s2] = s1;
-		}
-	}
+void union_set(int s1, int s2, vector<int>& parent, vector<int>& rank) {
+    if (rank[s1] < rank[s2]) {
+        rank[s2] += rank[s1];
+        parent[s1] = s2;
+    } else {
+        rank[s1] += rank[s2];
+        parent[s2] = s1;
+    }
 }
 
-bool sortcol( const vector<int>& v1, const vector<int>& v2 ) {
-	return v1[2] < v2[2];
+bool com(const vector<int>& v1, const vector<int>& v2) {
+    return v1[2] < v2[2];
 }
-int kruskal_algo(vector<vector<int>>&edgelist, int n)
-{
-	vector<int>parent(n, -1);
-	vector<int>rank(n, 1);
 
-	int ans = 0;
+void MST(vector<vector<int>>& edges, int n, int& ans) {
+    vector<int> parent(n, -1);
+    vector<int> rank(n, 1);
 
-	sort(edgelist.begin(), edgelist.end(), sortcol);
+    sort(edges.begin(), edges.end(), com);
 
-	for (auto edge : edgelist)
-	{
-		int x = edge[0], y = edge[1], wt = edge[2];
+    for (auto x : edges) {
+        int s1 = find_set(x[0], parent);
+        int s2 = find_set(x[1], parent);
 
-		int s1 = find(x, parent);
-		int s2 = find(y, parent);
-		if (s1 != s2)
-		{
-			Union(s1, s2, parent, rank);
-			ans += wt;
-		}
-	}
-	return ans;
+        if (s1 != s2) {
+            union_set(s1, s2, parent, rank);
+            ans += x[2];
+        }
+    }
 }
-int main()
-{
-	int n, m;
-	cin >> n >> m;
 
-	vector<vector<int>> edgelist;
+int main() {
+    int n, m, a, b, wt;
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b, wt;
-		cin >> a >> b >> wt;
-		edgelist.push_back({a, b, wt});
-	}
+    cin >> n >> m;
+    vector<vector<int>> edges;
 
-	cout << kruskal_algo(edgelist, n) << endl;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> wt;
+        edges.push_back({a, b, wt});
+    }
 
+    int ans = 0;
+    MST(edges, n, ans);
+
+    cout << "Weight of Minimum Spanning Tree: " << ans << endl;
+    return 0;
 }
