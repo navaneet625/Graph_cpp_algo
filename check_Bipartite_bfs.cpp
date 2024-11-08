1054b9a6dd4319fb5fe7f680e94af5cc35ef6afe
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-08 15:28:11 +0530

    Update check_Bipartite_bfs.cpp

--- check_Bipartite_bfs.cpp
+++ check_Bipartite_bfs.cpp
@@ -1,62 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool bfs(vector<vector<int>>&adj, int n)
-{
-	vector<int>color(n, -1);
+bool bfs(unordered_map<int, vector<int>>& adj) {
+    unordered_map<int, int> color;  // Map to store the color of each node
+    queue<pair<int, int>> Q;
 
-	queue<pair<int, int>>Q;
+    for (auto& node : adj) {  // Iterate through all nodes in the adjacency map
+        int start = node.first;
+        if (color.find(start) == color.end()) {  // If the node is unvisited
+            Q.push({start, 0});
+            color[start] = 0;
 
-	for (int i = 0; i < n; i++)
-	{
-		if (color[i] == -1)
-		{
-			Q.push({i, 0})
-			color[i] = 0;
-
-			while (!Q.empty())
-			{
-				int node = Q.front().first;
-				int c = Q.front().second;
-				Q.pop();
-
-				for (auto x : adj[node])
-				{
-					if (color[x] == c)
-						return false;
-					if (color[x] == -1)
-					{
-						color[x] = 1 - c;
-						Q.push({x, 1 - c});
-					}
-				}
-			}
-		}
-
-	}
-	return true;;
+            while (!Q.empty()) {
+                int node = Q.front().first;
+                int c = Q.front().second;
+                Q.pop();
+                for (auto x : adj[node]) {
+                    if (color.find(x) != color.end() && color[x] == c)
+                        return false;
+                    if (color.find(x) == color.end()) {  // If the neighbor is unvisited
+                        color[x] = 1 - c;
+                        Q.push({x, 1 - c});
+                    }
+                }
+            }
+        }
+    }
+    return true;
 }
 
-int main()
-{
-	int n, m;
-	cin >> n >> m;
+int main() {
+    int m, a, b;
+    cin >> m;
 
-	vector<vector<int>> adj(n);
+    unordered_map<int, vector<int>> adj;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-	vector<int>color(n, -1);
-
-	if (bfs(adj, n))
-		cout << "Yes\n";
-	else
-		cout << "No\n";
-
-	return 0;
+    bfs(adj)?cout << "Yes\n":cout << "No\n";
+    return 0;
 }
