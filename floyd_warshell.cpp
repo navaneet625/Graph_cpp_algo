4fcc8089d0ba315bbb0fff2612c813b27d224a0f
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-08 15:43:56 +0530

    Update floyd_warshell.cpp

--- floyd_warshell.cpp
+++ floyd_warshell.cpp
@@ -1,16 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector < vector<int>> floyd_warshell(vector<vector<int>>graph)
-{
+vector < vector<int>> floyd_warshell(vector<vector<int>>graph){
 	vector<vector<int>>dist(graph);
 	int n = graph.size();
-	for (int k = 0; k < n; k++)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
+	for (int k = 0; k < n; k++){
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
 				if (dist[i][j] > dist[i][k] + dist[k][j] && (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX))
 					dist[i][j] = dist[i][k] + dist[k][j];
 			}
@@ -19,8 +15,7 @@ vector < vector<int>> floyd_warshell(vector<vector<int>>graph)
 	return dist;
 }
 
-int main()
-{
+int main(){
 	vector<vector<int>> graph = {
 		{0, INT_MAX, -2, INT_MAX},
 		{4, 0, 3, INT_MAX},
@@ -30,11 +25,13 @@ int main()
 
 	auto dist = floyd_warshell(graph);
 	int n = dist.size();
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << dist[i][j] << " ";
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			if (dist[i][j] == INT_MAX) {
+                		cout << "INF ";
+            		} else {
+                		cout << dist[i][j] << " ";
+            		}
 		}
 		cout << endl;
 	}
