3ea7c155428d02690d66e516d66a4d6f61bc42f4
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-08 17:19:32 +0530

    Update shortest_cycle_bfs.cpp

--- shortest_cycle_bfs.cpp
+++ shortest_cycle_bfs.cpp
@@ -1,54 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void bfs(vector<vector<int>> &adj, int src, int n, int &ans)
-{
-	vector<int> dist(n + 1, INT_MAX);
-	queue<int> Q;
-	Q.push(src);
-	dist[src] = 0;
+void bfs(unordered_map<int, vector<int>>& adj, int src, int& ans) {
+    unordered_map<int, int> dist;  // Distance map for each node
+    queue<int> Q;
+    Q.push(src);
+    dist[src] = 0;
 
-	while (!Q.empty())
-	{
-		int node = Q.front();
-		Q.pop();
-		for (auto nbr : adj[node])
-		{
-			if (dist[nbr] == INT_MAX)
-			{
-				Q.push(nbr);
-				dist[nbr] = dist[node] + 1;
-			}
-			else if (dist[nbr] >= dist[node]) // backedges..
-			{
-				ans = min(ans, dist[nbr] + dist[node] + 1);
-			}
-		}
-	}
+    while (!Q.empty()) {
+        int node = Q.front();
+        Q.pop();
+
+        for (auto nbr : adj[node]) {
+            if (dist.find(nbr) == dist.end()) {  // Not visited
+                Q.push(nbr);
+                dist[nbr] = dist[node] + 1;
+            }
+            // Detecting a cycle through a back edge
+            else if (dist[nbr] >= dist[node]) {
+                ans = min(ans, dist[nbr] + dist[node] + 1);
+            }
+        }
+    }
 }
 
-int main()
-{
-	int n, m;
-	cin >> n >> m;
+int main() {
+    int m;
+    cout << "Enter the number of edges: ";
+    cin >> m;
 
-	vector<vector<int>> adj(n + 1);
+    unordered_map<int, vector<int>> adj;
+    cout << "Enter each edge as: source destination\n";
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
+    int ans = INT_MAX;
+    unordered_set<int> visited_nodes;  // Set to track nodes we start BFS from
+    for (const auto& pair : adj) {
+        int node = pair.first;
+        if (visited_nodes.find(node) == visited_nodes.end()) {
+            visited_nodes.insert(node);
+            bfs(adj, node, ans);
+        }
+    }
 
-	int ans = n + 1;
-	for (int i = 0; i < n; i++)
-	{
-		bfs(adj, i, n, ans);
-	}
-	if (ans == n + 1)
-		cout << "Cycle note present int the given garph " << "\n";
-	else
-		cout << "Shortest Cycle length is :" << ans << "\n";
+    if (ans == INT_MAX)
+        cout << "Cycle not present in the given graph.\n";
+    else
+        cout << "Shortest Cycle length is: " << ans << "\n";
+    return 0;
 }
