29b58e09250329756022f7e0c8ee6d4384e725f7
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-08 17:27:57 +0530

    Update single_source_shortest_path_bfs.cpp

--- single_source_shortest_path_bfs.cpp
+++ single_source_shortest_path_bfs.cpp
@@ -1,42 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int n, m, source;
+int main() {
+    int m, source,a,b;
+    cout << "Enter the number of edges: ";
+    cin >> m;
 
-	cin >> n >> m;
+    unordered_map<int, vector<int>> adj;
+    cout << "Enter each edge as: node1 node2\n";
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
 
-	vector < vector<int>> adj(n + 1);
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-	cin >> source;
-	vector<int> vis(n + 1, -1);
+    cout << "Enter the source node: ";
+    cin >> source;
 
-	queue<int> Q;
+    unordered_map<int, int> vis;  // Distance from the source
+    queue<int> Q;
 
+    vis[source] = 0;  // Distance of source to itself is 0
+    Q.push(source);
 
-	vis[source] = 0;
-	Q.push(source);
+    while (!Q.empty()) {
+        int node = Q.front();
+        Q.pop();
+        for (auto x : adj[node]) {
+            if (vis.find(x) != vis.end())  // If already visited, skip
+                continue;
+            vis[x] = vis[node] + 1;
+            Q.push(x);
+        }
+    }
 
-	while (!Q.empty())
-	{
-		int node = Q.front();
-		Q.pop();
-		// cout << node << " ";
-		for (auto x : adj[node])
-		{
-			if (vis[x] != -1)
-				continue;
-			vis[x] = vis[node] + 1;
-			Q.push(x);
-		}
-	}
-	for (auto x : vis)
-		cout << x << " ";
+    // Output distances from the source node
+    cout << "Node distances from source:\n";
+    for (const auto &pair : vis) {
+        cout << "Node " << pair.first << ": " << pair.second << "\n";
+    }
+
+    return 0;
 }
