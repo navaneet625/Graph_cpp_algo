f2cdc6c5b44861beca405cc1502cde10b9be9ee2
Navneet Chauhan <42150021+navaneet625@users.noreply.github.com>
2024-11-08 15:00:31 +0530

    Update SSSP_Bellman_ford.cpp

--- SSSP_Bellman_ford.cpp
+++ SSSP_Bellman_ford.cpp
@@ -1,49 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void Bellman_ford(vector<vector<int>>&Edges, vector<int>&Dist, int n)
-{
-	//relax Edges n-1 times
-	for (int i = 0; i < n - 1; i++)
-	{
-		for (auto x : Edges)
-		{
-			int a = x[0], b = x[1], wt = x[2];
-			if (Dist[a] != INT_MAX && Dist[a] + wt < Dist[b])
-				Dist[b] = Dist[a] + wt;
-		}
-	}
-	for (auto x : Edges)
-	{
-		int a = x[0], b = x[1], wt = x[2];
-		if (Dist[a] != INT_MAX && Dist[a] + wt < Dist[b])
-		{
-			cout << "Negative wt cycle are there in the graph \n";
-			exit(0);
-		}
-	}
+void Bellman_ford(vector<vector<int>>& Edges, unordered_map<int, int>& Dist, int numNodes){
+    // Relax edges numNodes - 1 times
+    for (int i = 0; i < numNodes - 1; i++){
+        for (auto& x : Edges){
+            int a = x[0], b = x[1], wt = x[2];
+            if (Dist[a] != INT_MAX && Dist[a] + wt < Dist[b])
+                Dist[b] = Dist[a] + wt;
+        }
+    }
 
+    // Check for negative weight cycles
+    for (auto& x : Edges){
+        int a = x[0], b = x[1], wt = x[2];
+        if (Dist[a] != INT_MAX && Dist[a] + wt < Dist[b]){
+            cout << "Negative weight cycle detected in the graph\n";
+            exit(0);
+        }
+    }
 }
-int main()
-{
-	int n, m;
-	cin >> n >> m;
 
-	vector<vector<int>> Edges;
+int main(){
+    int m;
+    cin >> m;
 
-	for (int i = 0; i < m; i++)
-	{
-		int a, b, wt;
-		cin >> a >> b >> wt;
-		Edges.push_back({a, b, wt});
-	}
-	vector<int> Dist(n + 1, INT_MAX);
-	int src;
-	cin >> src;
-	Dist[src] = 0;
-	Bellman_ford(Edges, Dist, n);
+    vector<vector<int>> Edges;
+    unordered_map<int, int> Dist;  // Dynamic distance map
+    unordered_set<int> uniqueNodes;
+	
+    for (int i = 0; i < m; i++){
+        int a, b, wt;
+        cin >> a >> b >> wt;
+        Edges.push_back({a, b, wt});
+        uniqueNodes.insert(a);
+        uniqueNodes.insert(b);
 
-	for (auto x : Dist)
-		cout << x << " ";
-	return 0;
+        // Initialize distances for each node as INT_MAX if not already set
+        if (Dist.find(a) == Dist.end()) Dist[a] = INT_MAX;
+        if (Dist.find(b) == Dist.end()) Dist[b] = INT_MAX;
+    }
+
+    int src;
+    cin >> src;
+    Dist[src] = 0;
+
+    int numNodes = uniqueNodes.size();  // Get the actual number of unique nodes
+    Bellman_ford(Edges, Dist, numNodes);
+
+    // Output distances
+    for (auto& [node, distance] : Dist){
+        if (distance == INT_MAX)
+            cout << "Node " << node << ": INF\n";
+        else
+            cout << "Node " << node << ": " << distance << "\n";
+    }
+    return 0;
 }
